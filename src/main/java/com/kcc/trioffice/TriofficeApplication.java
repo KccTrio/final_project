38defb5ad90438872563a9d30b1e4f819b03e43a
woo0doo <duden5000@gmail.com>
2024-09-24 16:01:42 +0900

    init

---
+++ src/main/java/com/kcc/trioffice/TriofficeApplication.java
@@ -0,0 +1,13 @@
+package com.kcc.trioffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcc/trioffice/TriofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcc.trioffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
