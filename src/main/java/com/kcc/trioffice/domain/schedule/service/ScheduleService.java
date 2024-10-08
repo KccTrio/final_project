a03e5068652eaf42808383fc2a6c8bf0f5df1bdb
Seong_gu <sunggu507@naver.com>
2024-10-08 16:31:43 +0900

    fix: google password hide

---
+++ src/main/java/com/kcc/trioffice/domain/schedule/service/ScheduleService.java
@@ -0,0 +1,11 @@
+package com.kcc.trioffice.domain.schedule.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+
+}
