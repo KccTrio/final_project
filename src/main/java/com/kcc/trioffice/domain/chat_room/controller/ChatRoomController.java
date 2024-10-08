0b58189dd7a6baba7681b7aeb1838f2b52421e69
woo0doo <duden5000@gmail.com>
2024-10-08 16:06:20 +0900

    feat: controller 로그인 한 사용자로 변경

--- src/main/java/com/kcc/trioffice/domain/chat_room/controller/ChatRoomController.java
+++ src/main/java/com/kcc/trioffice/domain/chat_room/controller/ChatRoomController.java
@@ -3,8 +3,10 @@ package com.kcc.trioffice.domain.chat_room.controller;
 import com.kcc.trioffice.domain.chat_room.dto.request.ChatRoomCreate;
 import com.kcc.trioffice.domain.chat_room.dto.response.ChatRoomInfo;
 import com.kcc.trioffice.domain.chat_room.service.ChatRoomService;
+import com.kcc.trioffice.global.auth.PrincipalDetail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +23,9 @@ public class ChatRoomController {
     private final ChatRoomService chatRoomService;
 
     @GetMapping("/chatrooms")
-    public String chatRoomList(Model model) {
-        List<ChatRoomInfo> chatRoomList = chatRoomService.getChatRoomList(1L);
+    public String chatRoomList(Model model,
+                               @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        List<ChatRoomInfo> chatRoomList = chatRoomService.getChatRoomList(principalDetail.getEmployeeId());
 
         model.addAttribute("chatRoomList", chatRoomList);
         return "chat_room/chat-room-list";
@@ -34,8 +37,9 @@ public class ChatRoomController {
     }
 
     @PostMapping("/chatrooms/save")
-    public String saveChatRoom(@ModelAttribute ChatRoomCreate chatRoomCreate) { // 유저 아이디 세션에서 받아야 함
-        chatRoomService.createChatRoom(chatRoomCreate, 1L);
+    public String saveChatRoom(@ModelAttribute ChatRoomCreate chatRoomCreate,
+                               @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        chatRoomService.createChatRoom(chatRoomCreate, principalDetail.getEmployeeId());
 
         return "redirect:/chatrooms";
     }
