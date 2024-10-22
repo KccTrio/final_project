00776e5a66c4afb8ddac8437418e24436c0f3f2c
woo0doo <duden5000@gmail.com>
2024-10-22 10:49:22 +0900

    feat: cors 설정

--- src/main/java/com/kcc/trioffice/global/config/WebConfig.java
+++ src/main/java/com/kcc/trioffice/global/config/WebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
