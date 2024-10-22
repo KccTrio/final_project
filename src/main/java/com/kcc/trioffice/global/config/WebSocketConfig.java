1cf4218c4c844b9a7a1650c98c42891959da62e3
woo0doo <duden5000@gmail.com>
2024-10-22 11:08:00 +0900

    refactor: websocket cors

--- src/main/java/com/kcc/trioffice/global/config/WebSocketConfig.java
+++ src/main/java/com/kcc/trioffice/global/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
         registry.addEndpoint("/stomp/connection")
-                .setAllowedOrigins("http://localhost:8081")
+                .setAllowedOrigins("http://localhost:8081", "https://www.trioffice.site", "https://trioffice.site")
                 .withSockJS();
     }
 
