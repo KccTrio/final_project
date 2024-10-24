1c13b4eba2ada2ff5da0a9b5491d8b3aca5d604f
Seong_gu <sunggu507@naver.com>
2024-10-24 11:15:05 +0900

    feat: ✅spring ai test 되는 버전, seonggu local에서만 됨

--- src/main/java/com/kcc/trioffice/global/chat_bot/ChatBotConfig.java
+++ src/main/java/com/kcc/trioffice/global/chat_bot/ChatBotConfig.java
@@ -14,14 +14,9 @@ public class ChatBotConfig {
 
     // CustomChatOptions 인스턴스 생성
     CustomChatBotOptions options = new CustomChatBotOptions("gpt-3.5-turbo", 500) // 모델 이름 및 최대 토큰 수 설정
-        .setFrequencyPenalty(0.5) // 예시로 주어진 옵션 설정
-        .setPresencePenalty(0.5)
-        .setTemperature(0.7)
-        .setTopK(40)
-        .setTopP(0.9);
+    ;
 
     return builder
-        .defaultOptions(options)
         .build();
   }
 }
